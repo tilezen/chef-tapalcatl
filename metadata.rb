306df4717cc7b614f59bcd235f10b8d8ca6aeae7
Robert Marianski <rob@marianski.com>
2017-01-26 09:53:20 -0500

    Version bump -> 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'matt.amos@mapzen.com'
 license          'MIT'
 description      'Installs/Configures tapalcatl'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.1.0'
 
 recipe 'tapalcatl', 'Installs Tapalcatl'
 
